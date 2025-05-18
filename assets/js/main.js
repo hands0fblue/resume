c543639a6beeacc1cbf3fdfb05fc158c6051a0cc
hands0fblue <17346751+hands0fblue@users.noreply.github.com>
2025-05-18 12:49:20 -0500

    complete, but troubleshooting js.mainpdf maker

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,8 +46,8 @@ window.addEventListener('scroll', scrollActive)
 /*==================== SHOW SCROLL TOP ====================*/ 
 function scrollTop(){
     const scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollTop)
 
@@ -82,27 +82,43 @@ themeButton.addEventListener('click', () => {
 })
 
 /*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/ 
-
+function scaleCV(){
+    document.body.classList.add('scale-cv')
+}
 
 /*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/ 
-
+function removeScale(){
+    document.body.classList.remove('scale-cv')
+}
 
 /*==================== GENERATE PDF ====================*/ 
 // PDF generated area
+let area = document.getElementById('area-cv')
 
+let resumeButton = document.getElementById('resume-button')
 
 // Html2pdf options
-
+let opt = {
+    magin:          0,
+    filename:       'myResume.pdf',
+    image:          { type: 'jpeg', quality: 0.98},
+    html2canvas:    { scale: 4},
+    jsPDF:          { format: 'a4', orientation: 'portrait'}
+}
 
 // Function to call areaCv and Html2Pdf options 
-
+function generateResume(){
+    html2pdf(areaCv)
+}
 
 // When the button is clicked, it executes the three functions
-
+resumeButton.addEventListener('click', () =>{
     // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-
+    scaleCV()
 
     // 2. The PDF is generated
-
+    generateResume()
 
     // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    setTimeout(removeScale, 5000)
+})
